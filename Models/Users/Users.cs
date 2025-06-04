32302427ef16db69b53ea6bbe19af6792556e238
andr0meda727 <kwwk747@gmail.com>
2025-06-04 13:38:45 +0200

    Add User entity

---
+++ Models/Users/Users.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cinema_Management_System.Models.Users
+{
+    public enum Role
+    {
+        Admin,
+        Employee,
+        Customer
+    }
+
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+       
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; } = string.Empty;
+
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        [Required]
+        public Role Role { get; set; } = Role.Customer;
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
