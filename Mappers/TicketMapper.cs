fcb2a26c44cac2050db5a0eb99811c06559056f1
kwwk <kwwk747@gmail.com>
2025-06-05 18:10:19 +0200

    Add BasicTicketDto
    It will be used to display quick information about user tickets (also those in the past).

---
+++ Mappers/TicketMapper.cs
@@ -0,0 +1,14 @@
+﻿using Cinema_Management_System.Models.Cinema;
+using Cinema_Management_System.DTOs;
+using Riok.Mapperly.Abstractions;
+
+namespace Cinema_Management_System.Mappers
+{
+    [Mapper]
+    public partial class TicketMapper
+    {
+        [MapProperty(nameof(Ticket.Screening.Movie.Title), nameof(BasicTicketDto.MovieTitle))]
+        [MapProperty(nameof(Ticket.Screening.DateStartTime), nameof(BasicTicketDto.DateStartTime))]
+        public partial BasicTicketDto ToBasicDto(Ticket ticket);
+    }
+}
