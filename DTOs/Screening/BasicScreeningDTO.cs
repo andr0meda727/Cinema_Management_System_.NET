cf2900a18627369afa79bc43517d334244264497
andr0meda727 <kwwk747@gmail.com>
2025-06-07 01:19:27 +0200

    Add DTO with basic information for screening

---
+++ DTOs/Screening/BasicScreeningDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Cinema_Management_System.DTOs.Screening
+{
+    public class BasicScreeningDTO
+    {
+        public string Title { get; set; }
+        public DateTime DateStartTime { get; set; }
+        public string? MoviePosterUrl { get; set; }
+    }
+}
