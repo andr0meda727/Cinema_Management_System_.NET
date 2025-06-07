0eea31609f80a8000e7284d867375203328cfc12
Mateusz Wojtasz <mateuszwojtasz1@gmail.com>
2025-06-08 01:37:08 +0200

    Add Employee Panel routing
    added routing for panel and created placeholder files

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cinema_Management_System.Controllers
+{
+    [Authorize(Roles = "Employee")]
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); // widok: Views/Employee/Index.cshtml
+        }
+
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+
+        public IActionResult EditMovies()
+        {
+            return View();
+        }
+
+        public IActionResult DeleteMovie()
+        {
+            return View();
+        }
+
+        public IActionResult BrowseMovies()
+        {
+            return View();
+        }
+
+        public IActionResult AddHall()
+        {
+            return View();
+        }
+
+        public IActionResult EditHalls()
+        {
+            return View();
+        }
+
+        public IActionResult DeleteHall()
+        {
+            return View();
+        }
+
+        public IActionResult BrowseHalls()
+        {
+            return View();
+        }
+
+        public IActionResult AddScreening()
+        {
+            return View();
+        }
+
+        public IActionResult EditScreenings()
+        {
+            return View();
+        }
+
+        public IActionResult DeleteScreening()
+        {
+            return View();
+        }
+
+        public IActionResult BrowseScreenings()
+        {
+            return View();
+        }
+
+        public IActionResult TicketSalesReport()
+        {
+            return View();
+        }
+    }
+}
