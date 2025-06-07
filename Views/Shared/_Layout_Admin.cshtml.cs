3b4d4ee69c225b2fe278dfe5796269bc738229be
Mateusz Wojtasz <mateuszwojtasz1@gmail.com>
2025-06-08 00:13:14 +0200

    Split navbar based on access
    Admin, Guest, Employee, User have different navbar layouts

---
+++ Views/Shared/_Layout_Admin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cinema_Management_System.Views.Shared
+{
+    public class _Layout_AdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
