cea262a47fbfc7d32a93f211462268f07c8969b4
andr0meda727 <kwwk747@gmail.com>
2025-06-08 17:13:11 +0200

    Add DTOs for seat selection

---
+++ DTOs/Seat/SeatDTO.cs
@@ -0,0 +1,13 @@
+﻿using Cinema_Management_System.Models.Cinema;
+
+namespace Cinema_Management_System.DTOs.Seat
+{
+    public class SeatDTO
+    {
+        public string Id { get; set; }
+        public string Row { get; set; }
+        public int SeatInRow { get; set; }
+        public SeatTypes SeatType { get; set; }
+        public bool isTaken { get; set; } // 0 - free, 1 - occupied
+    }
+}
---
+++ DTOs/Seat/SeatSelectionDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Cinema_Management_System.DTOs.Seat
+{
+    public class SeatSelectionDTO
+    {
+        public int ScreeningId { get; set; }
+        public decimal BasePrice { get; set; }
+        public List<SeatDTO> Seats { get; set; }
+    }
+}
