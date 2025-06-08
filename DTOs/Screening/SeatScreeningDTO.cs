2233053421392eaf048c41ca793b093b0f646e5e
andr0meda727 <kwwk747@gmail.com>
2025-06-09 00:51:20 +0200

    Modify SeatSelectionMapper
    It sends data about Screening, Seats and BasePrice to the UI.
    Its used to display seat chooser for a specific screening.
    User is able to choose standard, vip and double seats.

---
+++ DTOs/Screening/SeatScreeningDTO.cs
@@ -0,0 +1,12 @@
+﻿using Cinema_Management_System.Models.Cinema;
+
+namespace Cinema_Management_System.DTOs.Screening
+{
+    public class SeatScreeningDTO
+    {
+        public int Id { get; set; }
+        public decimal BasePrice { get; set; }
+        public ScreeningRoom? ScreeningRoom { get; set; }
+    }
+
+}
--- Mappers/SeatSelectionMapper.cs
+++ Mappers/SeatSelectionMapper.cs
@@ -1,4 +1,5 @@
 ﻿using Cinema_Management_System.DTOs.Seat;
+using Cinema_Management_System.DTOs.Screening;
 using Cinema_Management_System.Models.Cinema;
 using Riok.Mapperly.Abstractions;
 
@@ -7,21 +8,31 @@ namespace Cinema_Management_System.Mappers;
 [Mapper]
 public partial class SeatSelectionMapper
 {
-    [MapProperty("SeatStatus", nameof(SeatDTO.isTaken))]
+    [MapProperty(nameof(Seat.SeatStatus), nameof(SeatDTO.isTaken))]
     [MapperIgnoreSource(nameof(Seat.ScreeningRoomId))]
     [MapperIgnoreSource(nameof(Seat.ScreeningRoom))]
     [MapperIgnoreSource(nameof(Seat.Ticket))]
+    [MapperIgnoreTarget(nameof(SeatDTO.Price))]
     public partial SeatDTO SeatToSeatDTO(Seat seat);
 
     public partial List<SeatDTO> MapSeats(List<Seat> seats);
 
-    [MapProperty("Id", nameof(SeatSelectionDTO.ScreeningId))]
     [MapperIgnoreSource(nameof(Screening.MovieId))]
     [MapperIgnoreSource(nameof(Screening.Movie))]
     [MapperIgnoreSource(nameof(Screening.DateStartTime))]
     [MapperIgnoreSource(nameof(Screening.DateEndTime))]
     [MapperIgnoreSource(nameof(Screening.ScreeningRoomId))]
-    [MapperIgnoreSource(nameof(Screening.ScreeningRoom))]
     [MapperIgnoreSource(nameof(Screening.Tickets))]
-    public partial SeatSelectionDTO ScreeningToSeatSelectionDTO(Screening screening, List<Seat> seats);
+    public partial SeatScreeningDTO ScreeningToSeatScreeningDTO(Screening screening);
+
+    public SeatSelectionDTO ScreeningToSeatSelectionDTO(Screening screening, List<Seat> seats)
+    {
+        var dto = new SeatSelectionDTO
+        {
+            Screening = ScreeningToSeatScreeningDTO(screening),
+            Seats = MapSeats(seats),
+            BasePrice = screening.BasePrice
+        };
+        return dto;
+    }
 }
