8d1b7181e81abcdc213dd9c0d22d3bf63c566b1f
Mateusz Wojtasz <mateuszwojtasz1@gmail.com>
2025-06-09 12:07:09 +0200

    Change Browsing Films
    Added details panel on listing films

---
+++ Controllers/Employee/DetailsController.cs
@@ -0,0 +1,28 @@
+﻿using Cinema_Management_System.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cinema_Management_System.Controllers.Employee
+{
+    [Authorize(Roles = "Employee")]
+
+    public class DetailsController : Controller
+    {
+        private readonly CinemaDbContext _db;
+
+        public DetailsController(CinemaDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var movie = await _db.Movies.FindAsync(id);
+            if (movie == null)
+                return NotFound();
+
+            return View("~/Views/Employee/Movie/Details.cshtml", movie);
+        }
+    }
+}
