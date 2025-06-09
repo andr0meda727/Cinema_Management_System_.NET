8abef424991cd2b108a9d596edbecbd60680a63f
Mateusz Wojtasz <mateuszwojtasz1@gmail.com>
2025-06-10 01:55:05 +0200

    Add Delete Screenings

---
+++ DTOs/Employee/DeleteScreeningDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Cinema_Management_System.DTOs.Employee
+{
+    public class DeleteScreeningDTO
+    {
+        public int Id { get; set; }
+        public string MovieTitle { get; set; }
+        public string RoomName { get; set; }
+        public DateTime DateStartTime { get; set; }
+    }
+}
